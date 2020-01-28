a4264a40f794a26a4ec560baf887687025ba132c
Marc Penninga <marcpenninga@gmail.com>
2020-01-28 10:52:33 +0100

    [autoinst] Don't create empty subdirs in TEXMF tree

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-rm -rf test/output\ dir
-mkdir -p test/output\ dir
-cd test/output\ dir
+rm -rf test/output
+mkdir -p test/output
+cd test/output
 for dirname in ../testfiles/*; do
-    ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+    ../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
 done
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
+../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
+../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
 cd ../..
