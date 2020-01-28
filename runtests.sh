6eb954d72ffc7f517c32bbdcd81f712ee5df2a39
Marc Penninga <marcpenninga@gmail.com>
2020-01-28 11:28:42 +0100

    [autoinst] Clean up console and log messages

--- runtests.sh
+++ runtests.sh
@@ -4,8 +4,8 @@ rm -rf test/output
 mkdir -p test/output
 cd test/output
 for dirname in ../testfiles/*; do
-    ../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+    ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
 done
-../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
-../../bin/autoinst -verbose -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
+../../bin/autoinst -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
+../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
 cd ../..
