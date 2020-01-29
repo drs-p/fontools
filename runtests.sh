bf16730dd82489a5db263314bf505ea3e553115b
Marc Penninga <marcpenninga@gmail.com>
2020-01-29 15:40:16 +0100

    [runtests.sh] Add test cases without Regular and Medium

--- runtests.sh
+++ runtests.sh
@@ -8,4 +8,6 @@ for dirname in ../testfiles/*; do
 done
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
+../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
+../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
 cd ../..
