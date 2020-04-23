1926365df87306ddc9bbe8a7b68cd33c05619eb8
Marc Penninga <marcpenninga@gmail.com>
2020-04-23 14:31:16 +0200

    [runtests.sh] Run tests (mostly) in parallel

--- runtests.sh
+++ runtests.sh
@@ -3,9 +3,10 @@
 rm -rf test/output
 mkdir -p test/output
 cd test/output
-for dirname in ../testfiles/*; do
-    ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
-done
+# for dirname in ../testfiles/*; do
+#     ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+# done
+parallel --citation ../../bin/autoinst -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
