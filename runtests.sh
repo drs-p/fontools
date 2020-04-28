02bf0264c07dbc25e3ccbc0574793767a5fe6b31
Marc Penninga <marcpenninga@gmail.com>
2020-04-28 09:59:49 +0200

    [autoinst] Really fix -inferiors bug this time

--- runtests.sh
+++ runtests.sh
@@ -3,11 +3,15 @@
 rm -rf test/output
 mkdir -p test/output
 cd test/output
-# for dirname in ../testfiles/*; do
-#     ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
-# done
-parallel --citation ../../bin/autoinst -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -inferiors -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
+if [ -x "$(command -v parallel)" ]; then
+    parallel --citation ../../bin/autoinst -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
+else
+    for dirname in ../testfiles/*; do
+        ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+    done
+fi
+../../bin/autoinst -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
+../../bin/autoinst -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
 ../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
