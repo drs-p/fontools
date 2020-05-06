7fad44109e4bf0399e43c54bd6d9557be666e746
Marc Penninga <marcpenninga@gmail.com>
2020-05-06 16:15:45 +0200

    [runtests.sh] Limit used encodings to OT1 and T1

--- runtests.sh
+++ runtests.sh
@@ -6,15 +6,15 @@ rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [ -x "$(command -v parallel)" ]; then
-    parallel $AUTOINST -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        $AUTOINST -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+        $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap "$dirname"/*.[ot]tf
     done
 fi
-$AUTOINST -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
-$AUTOINST -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
 cd ../..
