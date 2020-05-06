6522bdafe4045bdb0aac0b8d5860edfa0e91813e
Marc Penninga <marcpenninga@gmail.com>
2020-05-06 12:55:12 +0200

    [runtests.sh] Parameterize location of binary

--- runtests.sh
+++ runtests.sh
@@ -1,18 +1,20 @@
 #! /bin/sh
 
+AUTOINST=../../bin/autoinst
+
 rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [ -x "$(command -v parallel)" ]; then
-    parallel ../../bin/autoinst -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -verbose -target="$PWD" -noupdmap {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        ../../bin/autoinst -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
+        $AUTOINST -verbose -target="$PWD" -noupdmap "$dirname"/*.[ot]tf
     done
 fi
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
-../../bin/autoinst -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
-../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
-../../bin/autoinst -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
+$AUTOINST -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
+$AUTOINST -verbose -target="$PWD" -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
 cd ../..
