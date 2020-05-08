fcc2de7a3510f7841365832ebc022979f78cfd12
Marc Penninga <marcpenninga@gmail.com>
2020-05-08 11:44:34 +0200

    [testcases.txt] Add GaramondLibre

--- runtests.sh
+++ runtests.sh
@@ -6,14 +6,13 @@ rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [ -x "$(command -v parallel)" ]; then
-    parallel $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap "$dirname"/*.[ot]tf
+        $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors "$dirname"/*.[ot]tf
     done
 fi
-$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -defaultproportional -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -defaultproportional -inferiors none ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
 $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
 $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
 $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -noupdmap $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
