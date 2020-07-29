fdc39d96f875dc59d40a9a67c6f8794920f812f5
Marc Penninga <marcpenninga@gmail.com>
2020-07-29 10:00:30 +0200

    [runtests.sh] Add "-pfb-suffix" to test commands

--- runtests.sh
+++ runtests.sh
@@ -6,18 +6,15 @@ rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [ -x "$(command -v parallel)" ]; then
-    parallel $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix -inferiors {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors "$dirname"/*.[ot]tf
+        $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix -inferiors "$dirname"/*.[ot]tf
     done
 fi
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -defaultproportional ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -nfssweight=m=medium ../testfiles/franklingothic/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors=subs ../testfiles/lato/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix ../testfiles/librecaslon/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix ../testfiles/merriweathertext/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix ../testfiles/sourcecodepro/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -defaultproportional -pfb-suffix ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -nfssweight=m=medium -pfb-suffix ../testfiles/franklingothic/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors=subs -pfb-suffix ../testfiles/lato/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
+$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
 cd ../..
