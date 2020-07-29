d10e6b1001ac80949d966063be7ed87fe31732e5
Marc Penninga <marcpenninga@gmail.com>
2020-07-29 14:48:35 +0200

    [autoinst] Cleaned up fontname suffix for type1

--- runtests.sh
+++ runtests.sh
@@ -6,15 +6,15 @@ rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [ -x "$(command -v parallel)" ]; then
-    parallel $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix -inferiors {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        $AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix -inferiors "$dirname"/*.[ot]tf
+        $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors "$dirname"/*.[ot]tf
     done
 fi
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -defaultproportional -pfb-suffix ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -nfssweight=m=medium -pfb-suffix ../testfiles/franklingothic/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -inferiors=subs -pfb-suffix ../testfiles/lato/*.[ot]tf
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
-$AUTOINST -verbose -target="$PWD" -noupdmap -enc=OT1,T1 -pfb-suffix $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -defaultproportional -t1suffix -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -nfssweight=m=medium -t1suffix -inferiors ../testfiles/franklingothic/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -inferiors=subs -t1suffix ../testfiles/lato/*.[ot]tf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular')
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors $(ls ../testfiles/sourcecodepro/*.[ot]tf | egrep -iv 'regular|medium')
 cd ../..
