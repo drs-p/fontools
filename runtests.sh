23720ec335d8342974328c77626fdbed9ee3ec51
Marc Penninga <marcpenninga@gmail.com>
2024-01-03 14:54:42 +0100

    [autoinst] Speed up and reorganize tests

--- runtests.sh
+++ runtests.sh
@@ -2,22 +2,29 @@
 
 AUTOINST=../../bin/autoinst
 
+# Most testcases use -dryrun to speed up execution
+
 rm -rf test/output
 mkdir -p test/output
 cd test/output
 if [[ -x "$(command -v parallel)" ]]; then
-    parallel $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors {}/*.[ot]tf ::: ../testfiles/*
+    parallel $AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors {}/*.[ot]tf ::: ../testfiles/*
 else
     for dirname in ../testfiles/*; do
-        $AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors "$dirname"/*.[ot]tf
+        $AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors "$dirname"/*.[ot]tf
     done
 fi
 
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -defaultproportional -t1suffix -inferiors ../testfiles/cormorantgaramond/*.[ot]tf
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -nfssweight=m=medium -t1suffix -inferiors ../testfiles/franklingothic/*.[ot]tf
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors=subs      -t1suffix ../testfiles/lato/*.[ot]tf
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors -defaultproportional      -t1suffix ../testfiles/cormorantgaramond/*.[ot]tf
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors -nfssweight=m=medium      -t1suffix ../testfiles/franklingothic/*.[ot]tf
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors=subs                      -t1suffix ../testfiles/lato/*.[ot]tf
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors -nfsswidth=m=semiexpanded -t1suffix ../testfiles/leaguemono/*.[ot]tf
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors -nfssweight=m=medium      -t1suffix $(ls ../testfiles/saira/*.[ot]tf)
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors -noproportional           -t1suffix $(ls ../testfiles/saira/*.[ot]tf | egrep -iv 'regular|-italic')
+$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -inferiors                           -t1suffix $(ls ../testfiles/saira/*.[ot]tf | egrep -iv 'regular|-italic|medium')
 
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1 -nfssweight=m=medium -t1suffix -inferiors $(ls ../testfiles/saira/*.[ot]tf)
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1                      -t1suffix -inferiors $(ls ../testfiles/saira/*.[ot]tf | egrep -iv 'regular|-italic')
-$AUTOINST -dryrun -verbose -target="$PWD" -enc=OT1,T1                      -t1suffix -inferiors $(ls ../testfiles/saira/*.[ot]tf | egrep -iv 'regular|-italic|medium')
+# Run a few tests without -dryrun
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors ../testfiles/dolly/*.otf
+$AUTOINST -verbose -target="$PWD" -enc=OT1,T1 -t1suffix -inferiors ../testfiles/go/*.ttf
+
 cd ../..
